f2ca53b6a34c0e4dec003ea45bae5fab092d3bad
Jan Gutter <github@jangutter.com>
2023-11-12 19:34:48 +0000

    Add passwordcard lib
    * This hooks up the PasswordCard java lib from
      https://www.passwordcard.org/en
    * It hooks up some primitive rendering but it's still very broken

--- app/src/main/java/com/jangutter/passwordcard/MainFragment.kt
+++ app/src/main/java/com/jangutter/passwordcard/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.jangutter.passwordcard
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jangutter.passwordcard.databinding.FragmentMainBinding
+import org.pepsoft.passwordcard.PasswordCard
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,6 +26,26 @@ class MainFragment : Fragment() {
     ): View? {
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        val appContext = requireContext().applicationContext
+        var prefs = appContext.applicationContext.getSharedPreferences(
+            appContext.packageName + "_preferences",
+            Context.MODE_PRIVATE)
+
+        var cardNumber = prefs.getString("card_number", "0")
+        var cardID = cardNumber?.toLong(radix = 16) ?: 0
+        var digitsOnlyArea = prefs.getBoolean("digits_only_area", false)
+        var extraSymbols = prefs.getBoolean("extra_symbols", false)
+
+        var passwordCard = PasswordCard(cardID, digitsOnlyArea, extraSymbols)
+        var grid = passwordCard.getGrid()
+        var strings = grid.map{String(it)}
+        var lines = strings.mapIndexed{idx, value -> "$idx $value"}
+
+        binding.apply{
+            textviewFirst.text = lines.joinToString(separator = "\n")
+        }
+
         return binding.root
 
     }
