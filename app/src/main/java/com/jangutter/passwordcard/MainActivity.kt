971e27efef600af3615a3a2a4573def39f483247
Jan Gutter <github@jangutter.com>
2023-11-12 12:17:45 +0000

    Add Settings Activity
    * Add a Settings activity using the wizard
    * Hook it up to the settings menu from the template

--- app/src/main/java/com/jangutter/passwordcard/MainActivity.kt
+++ app/src/main/java/com/jangutter/passwordcard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jangutter.passwordcard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -39,7 +40,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
